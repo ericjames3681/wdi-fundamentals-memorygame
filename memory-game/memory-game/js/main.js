a71725bf67cdff5bc0f6a75f54c2e692fcc4d852
Eric Peterson <ericjamesp.2009@gmail.com>
2019-12-26 05:26:52 -0600

    created game logic

--- memory-game/memory-game/js/main.js
+++ memory-game/memory-game/js/main.js
@@ -40,7 +40,7 @@ function flipCard() {
 
 	cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
-	cardsInPlay.push(cards[cardId].rank);
+	cardsInPlay.push(cards[cardId]);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 	checkForMatch();
@@ -48,8 +48,9 @@ function flipCard() {
 
 function createBoard() {
 	for (let i = 0; i < cards.length; i++) {
-    	let cardElement = document.createElement('cardImage');
-		document.getElementById('game-board').setAttribute('cardElement', 'images/back.png');
+
+		const cardElement = document.createElement('img');
+		cardElement.setAttribute('src','images/back.png');
 		cardElement.setAttribute('data-id', i);
 		cardElement.addEventListener('click', flipCard);
 		document.getElementById('game-board').appendChild(cardElement);
