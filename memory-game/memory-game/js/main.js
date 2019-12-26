23450e3d0bfd1b7c61045abebba96e2f05c811a0
Eric Peterson <ericjamesp.2009@gmail.com>
2019-12-26 00:31:53 -0600

    created game logic

--- memory-game/memory-game/js/main.js
+++ memory-game/memory-game/js/main.js
@@ -26,6 +26,8 @@ const cardsInPlay = [];
 
 
 function checkForMatch(){
+
+	document.getElementById('game-board').setAttribute('src', cards[cardId].cardImage);
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 	  console.log("You found a match!");
 	} 
@@ -34,15 +36,26 @@ function checkForMatch(){
 	}
 }
 
-function flipCard(cardId) {
+function flipCard() {
 
+	cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
-	cardsInPlay.push(cards[cardId].rank)
+	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 	checkForMatch();
 }
 
-flipCard(0);
-flipCard(2);
+function createBoard() {
+	for (let i = 0; i < cards.length; i++) {
+    	let cardElement = document.createElement('src');
+		document.getElementById('game-board').setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+
+}
+
+createBoard();
 
